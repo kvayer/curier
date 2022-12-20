01b58575dd7081408c4f3ba40fe971e4861c9e8f
kvayer <114985157+kvayer@users.noreply.github.com>
2022-12-20 12:51:16 +0400

    i found the way

---
+++ tests.py
@@ -0,0 +1,10 @@
+class D:
+    def __init__(self):
+        self.name = "Ivan"
+
+    def test(self):
+        print(self.name)
+
+
+launch = D()
+launch.test()
--- world.py
+++ world.py
@@ -1,11 +1,11 @@
 import math
 
-
 class Point:
     #инициализация
     def __init__(self,x,y):
         self.x = x
         self.y = y
+        coords = x,y
     #метод вывода координат
     def get_coords(self):
         return f'{self.x},{self.y}'
@@ -22,48 +22,44 @@ class Point:
 
 class Order:
 
-    def __init__(self,number,x,y,weight,price):
-        self.number = number
+    def __init__(self,x,y,weight,price):
         self.point = Point(x,y)
         self.weight = weight
+        weight = weight
         self.price = price
 
     def order_coords(self):
         return self.point.get_coords()
 
-
 class Courier:
-
-    def __init__(self, name: str,x,y, capacity=10):
-        self.name = name
+    def __init__(self,x,y, capacity=10):
         self.point = Point(x,y)
         self.history = []
         self.capacity = capacity
+        capacity = capacity
 
-    def cpurier_coords(self):
+    def courier_coords(self):
         return self.point.get_coords
 
-# class Record:
-#     def __init__(self,name:str,from_point: Point,to_point: Point,time_start,time_finish):
-#         self.courier_name = name
-#         self.from_point = from_point
-#         self.to_point = to_point
-#         self.orders = []
-#         self.time_start = time_start
-#         self.time_finish = time_finish
-#     def move(self,time_start,time_finish,from_point,to_point):
-#         if self.history:
-#             rec = last_start +
+class World:
+    def distribution(self,orders,couriers):
+        for c_key in couriers:
+            min_dist = -1
+            for o_key in orders:
+                # if couriers[c_key].capacity <= orders[o_key].weight:
+                dist = (orders[o_key].point).get_dist_to_center()+(couriers[c_key].point).get_dist(orders[o_key].point)
+                print(dist)
 
 plan = {}
 
 orders ={
-    0: Order('A',4,7,0,500),
-    1: Order('B',4,8,5,500),
-    2: Order('C',4,0,10,1000)
+    'A': Order(4,0,5,500),
+    'B': Order(4,8,5,500),
+    'C': Order(4,0,10,1000)
 }
 couriers ={
-    0: Courier('Курьер 1',5,0,15),
-    1: Courier('Курьер 2',10,10,5)
+    'Курьер 1': Courier(5,0,15),
+    'Курьер 2': Courier(10,10,5)
 }
-print(orders[0].order_coords())
+
+World.distribution(10,orders,couriers)
