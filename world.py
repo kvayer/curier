d27795be0f21bea5d41b214ba1dde1b6bc72f26a
04.10.2022 12:34 <armaturoipoebalu@gmail.com>
2023-01-10 04:00:50 +0400

    i cant start the program

--- world.py
+++ world.py
@@ -1,29 +1,31 @@
 import math
 
+
 class Point:
-    #инициализация
-    def __init__(self,x,y):
+    # инициализация
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        coords = x,y
-    #метод вывода координат
+        coords = x, y
+
+    # метод вывода координат
     def get_coords(self):
         return f'{self.x},{self.y}'
 
-    #метод расчета от точки до точки
-    def get_dist(self,other_point):
-        return math.dist((self.x,self.y),(other_point.x,other_point.y))
+    # метод расчета от точки до точки
+    def get_dist(self, other_point):
+        return math.dist((self.x, self.y), (other_point.x, other_point.y))
 
     def get_dist_to_center(self):
         x0 = 0
         y0 = 0
-        return math.dist((self.x,self.y),(x0,y0))
+        return math.dist((self.x, self.y), (x0, y0))
 
 
 class Order:
 
-    def __init__(self,x,y,weight,price):
-        self.point = Point(x,y)
+    def __init__(self, x, y, weight, price):
+        self.point = Point(x, y)
         self.weight = weight
         weight = weight
         self.price = price
@@ -31,9 +33,10 @@ class Order:
     def order_coords(self):
         return self.point.get_coords()
 
+
 class Courier:
-    def __init__(self,x,y, capacity=10):
-        self.point = Point(x,y)
+    def __init__(self, x, y, capacity=10):
+        self.point = Point(x, y)
         self.history = []
         self.capacity = capacity
         capacity = capacity
@@ -41,25 +44,32 @@ class Courier:
     def courier_coords(self):
         return self.point.get_coords
 
+
 class World:
-    def distribution(self,orders,couriers):
+    def distribution(self, orders, couriers):
+        closest_courier = []
         for c_key in couriers:
             min_dist = -1
             for o_key in orders:
-                # if couriers[c_key].capacity <= orders[o_key].weight:
-                dist = (orders[o_key].point).get_dist_to_center()+(couriers[c_key].point).get_dist(orders[o_key].point)
-                print(dist)
+                coords = orders[o_key].point
+                if couriers[c_key].capacity <= orders[o_key].weight:
+                    dist = (orders[o_key].point).get_dist_to_center() + (couriers[c_key].point).get_dist(
+                        orders[o_key].point)
+                if dist < min_dist:
+                    min_dist = dist
+                    coords = orders[o_key].point
+            closest_courier[c_key] = min_dist
+        return closest_courier
 
 plan = {}
 
-orders ={
-    'A': Order(4,0,5,500),
-    'B': Order(4,8,5,500),
-    'C': Order(4,0,10,1000)
+orders = {
+    'A': Order(4, 0, 5, 500),
+    'B': Order(6, 0, 5, 500),
+    'C': Order(3, 0, 10, 1000)
 }
-couriers ={
-    'Курьер 1': Courier(5,0,15),
-    'Курьер 2': Courier(10,10,5)
+couriers = {
+    'Курьер 1': Courier(5, 0, 15)
+    #    'Курьер 2': Courier(10,10,5)
 }
-
-World.distribution(10,orders,couriers)
+World().distribution(orders, couriers)
